9b9fc5d02fb79fa72115922cc87d608247334fe0
vladimirVenkov <vladimir.ognyanov.venkov@gmail.com>
2018-10-10 05:29:53 +0300

    Map added button: to select contact

--- app/src/main/java/venkov/vladimir/googleapisdemotelerik/MapActivity.java
+++ app/src/main/java/venkov/vladimir/googleapisdemotelerik/MapActivity.java
@@ -3,6 +3,7 @@ package venkov.vladimir.googleapisdemotelerik;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,11 +13,18 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class MapActivity extends AppCompatActivity {
 
     public GoogleMap mGoogleMap;
     final double LATITUDE = 42.6912445620777;
     final double LONGITUDE = 23.330605030059818;
+
+    @BindView(R.id.btn_share_this_location)
+    Button mShareThisLocationButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,15 +53,21 @@ public class MapActivity extends AppCompatActivity {
                                     "Sorry! unable to create maps", Toast.LENGTH_SHORT)
                                     .show();
                         } else {
-                            MarkerOptions marker = new MarkerOptions().position(coordinates).title("Your current location");
+                            MarkerOptions marker = new MarkerOptions().position(coordinates).title("Your best Store location");
                             mGoogleMap.addMarker(marker);
 
                             CameraPosition cameraPosition = new CameraPosition.Builder().target(
-                                    coordinates).zoom(6).build();
+                                    coordinates).zoom(14).build();
 
                             mGoogleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
                         }
                     });
         }
+    }
+
+    @OnClick(R.id.btn_share_this_location)
+    public void openContactsClick() {
+//        Intent pickContactToShareThisLocation = new Intent(this, .class);
+//        startActivity(pickContactToShareThisLocation);
     }
 }
